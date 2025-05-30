2e546f07af6cb1adb8ef778f7b9befa81e6ba359
AbhishekSingh421Singh <73700379+AbhishekSingh421Singh@users.noreply.github.com>
2025-05-30 19:20:35 +0000

    CI/CD pipeline

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -8,19 +8,19 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.10.0/24"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.20.0/24"
   availability_zone = "us-east-1b"
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.0.30.0/24"
   availability_zone = "us-east-1c"
 }
 
