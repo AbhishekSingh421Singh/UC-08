2322a2f42908f8579880df240f620bfdaad3b010
AbhishekSingh421Singh <73700379+AbhishekSingh421Singh@users.noreply.github.com>
2025-05-30 19:16:00 +0000

    CI/CD pipeline

--- terraform1/outputs.tf
+++ terraform1/outputs.tf
@@ -1,9 +1,8 @@
-output "public_subnet_1_id" {
-  value = aws_subnet.public_1.id
-}
-
-output "public_subnet_2_id" {
-  value = aws_subnet.public_2.id
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
 }
 
 output "load_balancer_dns" {
