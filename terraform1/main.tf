3625787b3bacd6e35e508abd77401ac020647c10
AbhishekSingh421Singh <73700379+AbhishekSingh421Singh@users.noreply.github.com>
2025-05-30 19:22:07 +0000

    CI/CD pipeline

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -85,3 +85,29 @@ resource "aws_lb" "main" {
     aws_subnet.public_2.id
   ]
 }
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a route table for public subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Associate public_1 subnet with the route table
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate public_2 subnet with the route table
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
